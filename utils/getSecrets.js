2dae76eaec75fa99da6c6c70937527ccd343e924
Maksym <marsmeme2000@gmail.com>
2025-04-30 18:20:03 +0700

    Add i18next-browser-languagedetector and utils/getSecrets.js

--- utils/getSecrets.js
+++ utils/getSecrets.js
@@ -1,17 +1,17 @@
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
 
-const secretsManager = new SecretsManagerClient({
-  region: process.env.AWS_REGION || 'ap-southeast-2',
-});
+   const secretsManager = new SecretsManagerClient({
+     region: process.env.AWS_REGION || 'ap-southeast-2',
+   });
 
-export async function getSecrets() {
-  try {
-    const response = await secretsManager.send(
-      new GetSecretValueCommand({ SecretId: process.env.SECRETS_MANAGER_SECRET_NAME })
-    );
-    return JSON.parse(response.SecretString);
-  } catch (error) {
-    console.error('Error fetching secrets:', error);
-    throw new Error('Failed to fetch secrets');
-  }
-}
+   export async function getSecrets() {
+     try {
+       const response = await secretsManager.send(
+         new GetSecretValueCommand({ SecretId: process.env.SECRETS_MANAGER_SECRET_NAME })
+       );
+       return JSON.parse(response.SecretString);
+     } catch (error) {
+       console.error('Error fetching secrets:', error);
+       throw new Error('Failed to fetch secrets');
+     }
+   }
